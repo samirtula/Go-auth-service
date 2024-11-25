8b682435dd1364dc475a886eb84e5e7b6a24a97c
Самир Гахраманов <gakhramanov.samir@lab-company.ru>
2024-11-25 19:45:51 +0400

    logger

--- cmd/sso/main.go
+++ cmd/sso/main.go
@@ -1,11 +1,38 @@
 package main
 
 import (
-	"fmt"
+	"log/slog"
 	"sso/internal/config"
+	"os"
+)
+
+const (
+	envLocal = "local"
+	envDev = "dev"
+	envProd = "prod"
 )
 
 func main() {
 	cfg := config.MustLoad()
-	fmt.Println(cfg)
+	log := setupLogger(cfg.Env)
+	log.Info("starting application",
+		 slog.String("env", cfg.Env),
+		 slog.Any("cfg", cfg),
+		 slog.Int("port", cfg.GRPC.Port),
+		)
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envDev: 
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envProd: 
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	}
+
+	return log
 }
